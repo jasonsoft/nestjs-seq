10c65ffc78ad693f93e135cd91854015cac701b1
jason.song <jasonsoft.net@gmail.com>
2021-09-08 11:34:20 +0800

    Fix the error that properties are not assigned

--- lib/jasonsoft-seq.service.ts
+++ lib/jasonsoft-seq.service.ts
@@ -8,9 +8,11 @@ import { SeqLogLevel, SeqEvent } from 'seq-logging';
  * JasonSoft Seq Logger Service
  * Added by Jason.Song (成长的小猪) on 2021/07/05 16:59:39
  */
- @Injectable()
+@Injectable()
 export class JasonSoftSeqService {
-  constructor(@Inject(JASONSOFT_SEQ_LOGGER) private readonly seqLogger: SeqLogger) {}
+  constructor(
+    @Inject(JASONSOFT_SEQ_LOGGER) private readonly seqLogger: SeqLogger,
+  ) {}
 
   public verbose(messageTemplate: string, properties?: object) {
     this.commit('Verbose', messageTemplate, properties);
@@ -25,7 +27,7 @@ export class JasonSoftSeqService {
   }
 
   public warn(messageTemplate: string, properties?: object) {
-    this.commit('Warning', messageTemplate, properties) ;
+    this.commit('Warning', messageTemplate, properties);
   }
 
   public error(messageTemplate: string, properties?: object | Error) {
@@ -36,16 +38,27 @@ export class JasonSoftSeqService {
     this.commit('Fatal', messageTemplate, properties);
   }
 
-  private commit(level: SeqLogLevel, messageTemplate: string, properties?: any) {
-    let { stack, ...errorProps } = properties;
-    const seqEvent:SeqEvent = {
+  /**
+   * Fix the error that properties are not assigned
+   * Updated by Jason.Song (成长的小猪) on 2021/09/08 11:29:21
+   * @param level
+   * @param messageTemplate
+   * @param properties
+   */
+  private commit(
+    level: SeqLogLevel,
+    messageTemplate: string,
+    properties?: any,
+  ) {
+    let { stack, ...props } = properties || {};
+    const seqEvent: SeqEvent = {
       timestamp: new Date(),
       level,
       messageTemplate,
       properties: {
         serviceName: this.seqLogger.serviceName,
         hostname: os.hostname(),
-        ...properties
+        properties: { ...props },
       },
       exception: stack ? stack : undefined,
     };
